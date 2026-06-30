4b8fb48d6a486a26a69c81879ae1ce2be6f15d6b
Prashant Singh <prashantj4505@gmail.com>
2026-06-30 08:23:43 +0530

    Fixed routing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\DriveFile;
 use App\Models\Folder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
